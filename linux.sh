6bac760db7128a876a0eac0da62dc53a26ef848e
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-04-24 20:03:45 +0530

    Create linux.sh

---
+++ linux.sh
@@ -0,0 +1,27 @@
+g='\033[1;32m'
+p='\033[1;35m'
+clear
+echo -e "\033[1;32m"
+clear
+echo -e $g 'Please Wait ===+['$p'>              '$g']\'
+sleep 0.2
+pip install requests
+clear
+echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
+sleep 0.2
+pip2 install requests
+clear
+echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
+sleep 0.2
+pip2 install mechanize
+python3 -m pip install requests
+clear 
+echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
+sleep 0.4
+cd main
+nano users.txt 
+chmod +x report.py
+clear
+echo -e "$g+++++++++++>[$p Welcome to the new update$p$g]<+++++++++++++"
+sleep 1
+python report.py
