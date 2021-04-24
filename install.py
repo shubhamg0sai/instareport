196bb8d5a05a280abcf73a5df2a61c739bf9c8ba
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-04-24 20:08:44 +0530

    Update install.py

--- install.py
+++ install.py
@@ -1,2 +1,50 @@
 import marshal, base64
-exec(base64.b64decode("I0NZQkVSIE5BTUUgQkxBQ0stS0lMTEVSCiNHSVRIVUI6IGh0dHBzOi8vd3d3Lmluc3RhZ3JhbS5jb20vc2h1YmhhbV9nMHNhaW4KaW1wb3J0IG9zCkNvcnJlY3RVc2VybmFtZSA9ICJnMHNhaW4iCkNvcnJlY3RQYXNzd29yZCA9ICJpbnN0YSIKCmxvb3AgPSAndHJ1ZScKd2hpbGUgKGxvb3AgPT0gJ3RydWUnKToKICAgIHVzZXJuYW1lID0gcmF3X2lucHV0KCJcMDMzWzE7OTZtWyNdIFx4MWJbMDszNm0gRW50ZXIgVXNlcm5hbWVceDFiWzE7OTJt4p6kICIpCiAgICBpZiAodXNlcm5hbWUgPT0gQ29ycmVjdFVzZXJuYW1lKToKICAgIAlwYXNzd29yZCA9IHJhd19pbnB1dCgiXDAzM1sxOzk2bVsjXSBceDFiWzA7MzZtIEVudGVyIFBhc3N3b3JkXHgxYlsxOzkybeKepCAiKQogICAgICAgIGlmIChwYXNzd29yZCA9PSBDb3JyZWN0UGFzc3dvcmQpOgogICAgICAgICAgICBwcmludCAiTG9nZ2VkIGluIHN1Y2Nlc3NmdWxseSBhcyAiICsgdXNlcm5hbWUgI2ZiLWNsb25pbmctaWQgU0cKICAgICAgICAgICAgbG9vcCA9ICdmYWxzZScKICAgICAgICBlbHNlOgogICAgICAgICAgICBwcmludCAiV3JvbmcgcGFzc3dvcmQhIgogICAgICAgICAgICBvcy5zeXN0ZW0oJ3hkZy1vcGVuIGh0dHBzOi8vd3d3Lmluc3RhZ3JhbS5jb20vc2h1YmhhbV9nMHNhaW4vP2hsPWVuJykKICAgIGVsc2U6CiAgICAgICAgcHJpbnQgIldyb25nIHVzZXJuYW1lISIKICAgICAgICBvcy5zeXN0ZW0oJ3hkZy1vcGVuIGh0dHBzOi8vd3d3Lmluc3RhZ3JhbS5jb20vc2h1YmhhbV9nMHNhaW4vP2hsPWVuJykKaW1wb3J0IG9zLHRpbWUKIyMjIyMgRGV2IDogU2h1QmhhbWcwc2FpbiMjIyMjCmRlZiBtZW51KCk6CiAgICAgICAgYWN0aW9uKCkKCmRlZiBhY3Rpb24oKToKICAgICAgICBiY2ggPSByYXdfaW5wdXQoJ1xuICBkbyB5b3Ugd2FudCB0byBjb250aW51ZSB5L24gJykKICAgICAgICBpZiBiY2ggPT0nJzoKICAgICAgICAgICAgICAgIHByaW50ICJcMDMzWzE7OTFtRmlsbCBpbiBjb3JyZWN0bHkiCiAgICAgICAgICAgICAgICBvcy5zeXN0ZW0oJ3hkZy1vcGVuIGh0dHBzOi8vd3d3Lmluc3RhZ3JhbS5jb20vc2h1YmhhbV9nMHNhaW4vP2hsPWVuJykKICAgICAgICAgICAgICAgIGFjdGlvbigpICAgCiAgICAgICAgZWxpZiBiY2ggPT0ieSI6CiAgICAgICAgICAgICAgICBvcy5zeXN0ZW0oImNsZWFyIikKICAgICAgICAgICAgICAgIG9zLnN5c3RlbSgiYmFzaCBpbnN0YWxsLnNoIikKICAgICAgICBlbHNlOgogICAgICAgICAgICAgICAgcHJpbnQgJ1shXSBGaWxsIGluIGNvcnJlY3RseScKICAgICAgICAgICAgICAgIG9zLnN5c3RlbSgneGRnLW9wZW4gaHR0cHM6Ly93d3cuaW5zdGFncmFtLmNvbS9zaHViaGFtX2cwc2Fpbi8/aGw9ZW4nKQogICAgICAgICAgICAgICAgYWN0aW9uKCkgICAgICAgCgppZiBfX25hbWVfXyA9PSAnX19tYWluX18nOgogICAgICAgIG1lbnUoKQoKICAgICAgICAK"))
+exec(base64.b64decode("I0NZQkVSIE5BTUUgQkxBQ0stS0lMTEVSCiNHSVRIVUI6IGh0dHBzOi8vd3d3Lmluc3RhZ3JhbS5jb20vc2h1YmhhbV9nMHNhaW4KaW1wb3J0IG9zCkNvcnJlY3RVc2VybmFtZSA9ICJnMHNhaW4iCkNvcnJlY3RQYXNzd29yZCA9ICJSZXBvcnQiCgpsb29wID0gJ3RydWUnCndoaWxlIChsb29wID09ICd0cnVlJyk6CiAgICB1c2VybmFtZSA9IHJhd19pbnB1dCgiXDAzM1sxOzk2bVsjXSBceDFiWzA7MzZtIEVudGVyIFVzZXJuYW1lXHgxYlsxOzkybeKepCAiKQogICAgaWYgKHVzZXJuYW1lID09IENvcnJlY3RVc2VybmFtZSk6CiAgICAJcGFzc3dvcmQgPSByYXdfaW5wdXQoIlwwMzNbMTs5Nm1bI10gXHgxYlswOzM2bSBFbnRlciBQYXNzd29yZFx4MWJbMTs5Mm3inqQgIikKICAgICAgICBpZiAocGFzc3dvcmQgPT0gQ29ycmVjdFBhc3N3b3JkKToKICAgICAgICAgICAgcHJpbnQgIkxvZ2dlZCBpbiBzdWNjZXNzZnVsbHkgYXMgIiArIHVzZXJuYW1lICNmYi1jbG9uaW5nLWlkIFNHCiAgICAgICAgICAgIGxvb3AgPSAnZmFsc2UnCiAgICAgICAgZWxzZToKICAgICAgICAgICAgcHJpbnQgIldyb25nIHBhc3N3b3JkISIKICAgICAgICAgICAgb3Muc3lzdGVtKCd4ZGctb3BlbiBodHRwczovL3d3dy5pbnN0YWdyYW0uY29tL3NodWJoYW1fZzBzYWluLz9obD1lbicpCiAgICBlbHNlOgogICAgICAgIHByaW50ICJXcm9uZyB1c2VybmFtZSEiCiAgICAgICAgb3Muc3lzdGVtKCd4ZGctb3BlbiBodHRwczovL3d3dy5pbnN0YWdyYW0uY29tL3NodWJoYW1fZzBzYWluLz9obD1lbicpCmltcG9ydCBvcyx0aW1lCiMjIyMjIERldiA6IFNodUJoYW1nMHNhaW4jIyMjIwpkZWYgbWVudSgpOgogICAgICAgIGFjdGlvbigpCgpkZWYgYWN0aW9uKCk6CiAgICAgICAgYmNoID0gcmF3X2lucHV0KCdcbiAgZG8geW91IHdhbnQgdG8gY29udGludWUgeS9uICcpCiAgICAgICAgaWYgYmNoID09Jyc6CiAgICAgICAgICAgICAgICBwcmludCAiXDAzM1sxOzkxbUZpbGwgaW4gY29ycmVjdGx5IgogICAgICAgICAgICAgICAgb3Muc3lzdGVtKCd4ZGctb3BlbiBodHRwczovL3d3dy5pbnN0YWdyYW0uY29tL3NodWJoYW1fZzBzYWluLz9obD1lbicpCiAgICAgICAgICAgICAgICBhY3Rpb24oKSAgIAogICAgICAgIGVsaWYgYmNoID09InkiOgogICAgICAgICAgICAgICAgb3Muc3lzdGVtKCJjbGVhciIpCiAgICAgICAgICAgICAgICBvcy5zeXN0ZW0oImJhc2ggdGVybXV4LnNoIikKICAgICAgICBlbHNlOgogICAgICAgICAgICAgICAgcHJpbnQgJ1shXSBGaWxsIGluIGNvcnJlY3RseScKICAgICAgICAgICAgICAgIG9zLnN5c3RlbSgneGRnLW9wZW4gaHR0cHM6Ly93d3cuaW5zdGFncmFtLmNvbS9zaHViaGFtX2cwc2Fpbi8/aGw9ZW4nKQogICAgICAgICAgICAgICAgYWN0aW9uKCkgICAgICAgCgppZiBfX25hbWVfXyA9PSAnX19tYWluX18nOgogICAgICAgIG1lbnUoKQoKICAgICAgICAK"))
+
+
+
+
+
+#CYBER NAME BLACK-KILLER
+#GITHUB: https://www.instagram.com/shubham_g0sain
+import os
+CorrectUsername = "g0sain"
+CorrectPassword = "Report"
+
+loop = 'true'
+while (loop == 'true'):
+    username = raw_input("\033[1;96m[#] \x1b[0;36m Enter Username\x1b[1;92m➤ ")
+    if (username == CorrectUsername):
+    	password = raw_input("\033[1;96m[#] \x1b[0;36m Enter Password\x1b[1;92m➤ ")
+        if (password == CorrectPassword):
+            print "Logged in successfully as " + username #fb-cloning-id SG
+            loop = 'false'
+        else:
+            print "Wrong password!"
+            os.system('xdg-open https://www.instagram.com/shubham_g0sain/?hl=en')
+    else:
+        print "Wrong username!"
+        os.system('xdg-open https://www.instagram.com/shubham_g0sain/?hl=en')
+import os,time
+##### Dev : ShuBhamg0sain#####
+def menu():
+        action()
+
+def action():
+        bch = raw_input('\n  do you want to continue y/n ')
+        if bch =='':
+                print "\033[1;91mFill in correctly"
+                os.system('xdg-open https://www.instagram.com/shubham_g0sain/?hl=en')
+                action()   
+        elif bch =="y":
+                os.system("clear")
+                os.system("bash termux.sh")
+        else:
+                print '[!] Fill in correctly'
+                os.system('xdg-open https://www.instagram.com/shubham_g0sain/?hl=en')
+                action()       
+
+if __name__ == '__main__':
+        menu()
+
+        
